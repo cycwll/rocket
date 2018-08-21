715b0b147ebfb9784cef77c1d36ef5e9990c1123
Chai Yingchao <yingchao.chai@capgemini.com>
2018-08-21 16:13:14 +0000

    chai

--- getImage.sh
+++ getImage.sh
@@ -2,5 +2,5 @@
 
 DATE=$(date +"%Y-%m-%d_%H%M")
 
-#fswebcam -r 1280x720 --no-banner /home/pi/webcam/$DATE.jpg
+fswebcam -r 1280x720 --no-banner /home/pi/webcam/$DATE.jpg
 #fswebcam -r 420x180 --no-banner /home/pi/webcam/$DATE.jpg
