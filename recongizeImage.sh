45a488662ef035f51ec43a1e80311c1247b8fe49
Chai Yingchao <yingchao.chai@capgemini.com>
2018-08-21 16:08:02 +0000

    chaiyingchao commit

--- recongizeImage.sh
+++ recongizeImage.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 cd /home/pi/webcam/models-master/tutorials/image/imagenet
 IMAGENAME=`ls -lt /home/pi/webcam|sed -n "2,1p"|awk '{print $9}'`
-python classify_image.py --image_file=/home/pi/webcam/$IMAGENAME > /tmp/image.txt && cat /tmp/image.txt | head -n 1|awk -F ',' '{print $1}' |awk '{print $1}' >/tmp/object.txt
-cat /tmp/object.txt
+#python classify_image.py --image_file=/home/pi/webcam/$IMAGENAME > /tmp/image.txt && cat /tmp/image.txt | head -n 1|awk -F ',' '{print $1}' |awk '{print $1}' >/tmp/object.txt
+python classify_image.py --image_file=/home/pi/webcam/$IMAGENAME > /tmp/image.txt 
+cat /tmp/image.txt
+#cat /tmp/image.txt|head -n 1|awk -F '(' '{print $1}' | espeak  2>/dev/null
